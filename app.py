a8007b0bf13b4e5bceb244366ebccadc54a7fd90
hallan-n <hallanneves01@gmail.com>
2023-08-12 01:28:55 -0300

    Finish API

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
+from itertools import count
 from flask import Flask, request, jsonify
 from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 from tinydb import TinyDB, Query
+from tinydb.storages import MemoryStorage
 
 app = Flask(__name__)
 spec = FlaskPydanticSpec("api-flask", title="api-flask")
 spec.register(app)
-database = TinyDB("database.json")
+database = TinyDB(storage=MemoryStorage)
+c = count()
 
 
 class User (BaseModel):
-    id: Optional[int]
+    id: Optional[int] = Field(default_factory=lambda: next(c))
     name: str
     age: int
 
@@ -33,29 +36,40 @@ def get_users():
     )
 
 
+@app.get("/users/<int:id>")
+@spec.validate(resp=Response(HTTP_200=User))
+def get_user(id):
+    """Retorna o User do id especificado"""
+    try:
+        user = database.search(Query().id == id)[0]
+        return jsonify(user)
+    except IndexError:
+        return {"message": "User Not Found"}, 404
+
+
 @app.post("/users")
-@spec.validate(body=Request(User), resp=Response(HTTP_200=User))
+@spec.validate(body=Request(User), resp=Response(HTTP_201=User))
 def set_user():
     """Insere um user no banco de dados"""
     body = request.context.body.dict()
     database.insert(body)
     return body
 
+
 @app.put("/users/<int:id>")
-@spec.validate(body=Request(User), resp=Response(HTTP_200=User))
+@spec.validate(body=Request(User), resp=Response(HTTP_201=User))
 def update_user(id):
     """Atualiza um user no banco de dados"""
-    User = Query()
     body = request.context.body.dict()
-    database.update(body, User.id == id)
+    database.update(body, Query().id == id)
     return jsonify(body)
 
+
 @app.delete("/users/<int:id>")
-@spec.validate(resp=Response(HTTP_200=User))
+@spec.validate(resp=Response("HTTP_204"))
 def delete_user(id):
     """Deleta um user no banco de dados"""
-    User = Query()
-    database.remove(User.id == id)
+    database.remove(Query().id == id)
     return jsonify({})
 
 
