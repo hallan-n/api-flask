1fcdd967cb49c295b1cb3fa442b276ca2edd2f31
hallan-n <hallanneves01@gmail.com>
2023-08-11 22:59:13 -0300

    Create Swagger and principal schema

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_pydantic_spec import FlaskPydanticSpec, Response
+from pydantic import BaseModel
 
-app= Flask(__name__)
+app = Flask(__name__)
+spec = FlaskPydanticSpec("flask", title="api-flask")
+spec.register(app)
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+
 
 @app.get("/users")
+@spec.validate(resp=Response(HTTP_200=User))
 def get_users():
     return "Ok"
 
